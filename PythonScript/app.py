8de543292926b154495738cab694a7fb389d9c09
EwanMcNeil <ewanmcneil11@gmail.com>
2021-02-20 22:32:28 -0500

    moved window out

--- PythonScript/app.py
+++ PythonScript/app.py
@@ -302,7 +302,7 @@ def UI_thread():
         print("STARTED UI")
         uiCreation = True
         app = QApplication(sys.argv)
-        win = Window()
+       
         win.droneSearchText.setStyleSheet("background-color:yellow")
         win.show()
         app.exec()
@@ -323,6 +323,7 @@ messageFlag = False
 uiCreation = False
 
 uiThread = threading.Thread(target =UI_thread)
+win = Window()
 
 if __name__ == "__main__":
    

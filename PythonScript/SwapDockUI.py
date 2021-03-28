0c70bc1b13c7c539feb4028b787c4c68669df91f
EwanMcNeil <ewanmcneil11@gmail.com>
2021-03-27 21:46:55 -0400

    added more stages in doc progress

--- PythonScript/SwapDockUI.py
+++ PythonScript/SwapDockUI.py
@@ -15,49 +15,71 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.droneSearchText = QtWidgets.QTextEdit(self.centralwidget)
-        self.droneSearchText.setGeometry(QtCore.QRect(10, 60, 131, 51))
+        self.droneSearchText.setGeometry(QtCore.QRect(170, 50, 131, 51))
         self.droneSearchText.setStyleSheet("background-color: rgb(255, 0, 0);\n"
 "alternate-background-color: rgb(0, 255, 127);\n"
 "alternate-background-color: rgb(255, 255, 127);")
         self.droneSearchText.setObjectName("droneSearchText")
         self.progressBar_search = QtWidgets.QProgressBar(self.centralwidget)
         self.progressBar_search.setEnabled(False)
-        self.progressBar_search.setGeometry(QtCore.QRect(160, 90, 118, 23))
+        self.progressBar_search.setGeometry(QtCore.QRect(320, 80, 118, 23))
         self.progressBar_search.setProperty("value", 24)
         self.progressBar_search.setObjectName("progressBar_search")
         self.centerTaskTextEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.centerTaskTextEdit.setGeometry(QtCore.QRect(10, 130, 131, 51))
+        self.centerTaskTextEdit.setGeometry(QtCore.QRect(170, 120, 131, 51))
         self.centerTaskTextEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
 "alternate-background-color: rgb(0, 255, 127);\n"
 "alternate-background-color: rgb(255, 255, 127);")
         self.centerTaskTextEdit.setObjectName("centerTaskTextEdit")
-        self.AlignmentTaskTextEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.AlignmentTaskTextEdit.setGeometry(QtCore.QRect(10, 200, 131, 51))
-        self.AlignmentTaskTextEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
+        self.RotatingDroneTextEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.RotatingDroneTextEdit.setGeometry(QtCore.QRect(170, 190, 131, 51))
+        self.RotatingDroneTextEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
 "alternate-background-color: rgb(0, 255, 127);\n"
 "alternate-background-color: rgb(255, 255, 127);")
-        self.AlignmentTaskTextEdit.setObjectName("AlignmentTaskTextEdit")
+        self.RotatingDroneTextEdit.setObjectName("RotatingDroneTextEdit")
         self.progressBar_center = QtWidgets.QProgressBar(self.centralwidget)
         self.progressBar_center.setEnabled(False)
-        self.progressBar_center.setGeometry(QtCore.QRect(160, 150, 118, 23))
+        self.progressBar_center.setGeometry(QtCore.QRect(320, 140, 118, 23))
         self.progressBar_center.setProperty("value", 24)
         self.progressBar_center.setObjectName("progressBar_center")
         self.progressBar_aligment = QtWidgets.QProgressBar(self.centralwidget)
         self.progressBar_aligment.setEnabled(False)
-        self.progressBar_aligment.setGeometry(QtCore.QRect(160, 230, 118, 23))
+        self.progressBar_aligment.setGeometry(QtCore.QRect(320, 220, 118, 23))
         self.progressBar_aligment.setProperty("value", 24)
         self.progressBar_aligment.setObjectName("progressBar_aligment")
-        self.ArmTaskTestEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.ArmTaskTestEdit.setGeometry(QtCore.QRect(10, 280, 131, 51))
-        self.ArmTaskTestEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
+        self.retrievingBatteryTestEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.retrievingBatteryTestEdit.setGeometry(QtCore.QRect(170, 340, 131, 51))
+        self.retrievingBatteryTestEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
 "alternate-background-color: rgb(0, 255, 127);\n"
 "alternate-background-color: rgb(255, 255, 127);")
-        self.ArmTaskTestEdit.setObjectName("ArmTaskTestEdit")
+        self.retrievingBatteryTestEdit.setObjectName("retrievingBatteryTestEdit")
         self.progressBar_arm = QtWidgets.QProgressBar(self.centralwidget)
         self.progressBar_arm.setEnabled(False)
-        self.progressBar_arm.setGeometry(QtCore.QRect(160, 300, 118, 23))
+        self.progressBar_arm.setGeometry(QtCore.QRect(320, 360, 118, 23))
         self.progressBar_arm.setProperty("value", 24)
         self.progressBar_arm.setObjectName("progressBar_arm")
+        self.lockingDroneTextEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.lockingDroneTextEdit.setGeometry(QtCore.QRect(170, 270, 131, 51))
+        self.lockingDroneTextEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
+"alternate-background-color: rgb(0, 255, 127);\n"
+"alternate-background-color: rgb(255, 255, 127);")
+        self.lockingDroneTextEdit.setObjectName("lockingDroneTextEdit")
+        self.placingBatteryTextEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.placingBatteryTextEdit.setGeometry(QtCore.QRect(170, 420, 131, 51))
+        self.placingBatteryTextEdit.setStyleSheet("background-color: rgb(255, 0, 0);\n"
+"alternate-background-color: rgb(0, 255, 127);\n"
+"alternate-background-color: rgb(255, 255, 127);")
+        self.placingBatteryTextEdit.setObjectName("placingBatteryTextEdit")
+        self.progressBar_arm_2 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_arm_2.setEnabled(False)
+        self.progressBar_arm_2.setGeometry(QtCore.QRect(320, 290, 118, 23))
+        self.progressBar_arm_2.setProperty("value", 24)
+        self.progressBar_arm_2.setObjectName("progressBar_arm_2")
+        self.progressBar_arm_3 = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar_arm_3.setEnabled(False)
+        self.progressBar_arm_3.setGeometry(QtCore.QRect(320, 430, 118, 23))
+        self.progressBar_arm_3.setProperty("value", 24)
+        self.progressBar_arm_3.setObjectName("progressBar_arm_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 27))
@@ -106,18 +128,30 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:600;\">Centering Task</span></p></body></html>"))
-        self.AlignmentTaskTextEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt; font-weight:600;\">Centering Drone</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.RotatingDroneTextEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:600;\">Alignment Task</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt; font-weight:600;\">Rotating Drone</span></p>\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:600;\"><br /></p></body></html>"))
-        self.ArmTaskTestEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.retrievingBatteryTestEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:600;\">Arm task</span></p></body></html>"))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt; font-weight:600;\">Retreiving Battery</span></p></body></html>"))
+        self.lockingDroneTextEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt; font-weight:600;\">Locking Drone</span></p></body></html>"))
+        self.placingBatteryTextEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt; font-weight:600;\">Rotating Drone</span></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:600;\"><br /></p></body></html>"))
         self.menuSettings.setTitle(_translate("MainWindow", "Settings"))
         self.menuArduino.setTitle(_translate("MainWindow", "Arduino"))
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
@@ -126,6 +160,4 @@ class Ui_MainWindow(object):
         self.actionRun_Centering.setText(_translate("MainWindow", "&Run Centering"))
         self.action_Run_Alignment.setText(_translate("MainWindow", "&Run Alignment"))
         self.action_Run_Arm.setText(_translate("MainWindow", "&Run Arm"))
-
-
 
